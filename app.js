99186f3c41bf0e6d0b93e1a6681911f019a478e3
Tglowria <tomiglows@gmail.com>
2024-01-25 10:32:52 +0100

    backend first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');});
+
+app.post("/post", (req, res) => {
+    const {userName} = req.body;
+    res.send(`Hello ${userName}`)
+});
+
+app.put("/put", (req, res) => {
+    res.send('This is a put request')
+})
+
+app.delete("/delete", (req, res) => {
+    res.send('This is a delete request')
+})
+
+app.patch("/patch", (req, res) => {
+    res.send('This is a patch request')
+})
+
+app.listen(port, ()=> {
+    console.log("Server is listening on port 4500");
+});
